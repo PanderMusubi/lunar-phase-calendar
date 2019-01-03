64ebd71e7bfdbdb85ceb34b26a0576bc24c884d6
PanderMusubi <pander@users.sourceforge.net>
2019-01-03 11:10:25 +0100

    i18n

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@
 
 from datetime import date, timedelta
 from astral import Astral
+from os.path import isdir
+from os import makedirs, chdir
 
 moon_phase_names = {'en':
                     ('New moon',
@@ -16,13 +18,37 @@ moon_phase_names = {'en':
                     ('Nieuwe maan',
                      'Wassende, sikkelvormige maan',
                      'Eerste kwartier',
-                    'Wassende,vooruitspringende maan',
+                     'Wassende,vooruitspringende maan',
                      'Volle maan',
                      'Krimpende, vooruitspringende maan',
                      'Laatste kwartier',
                      'Krimpende, sikkelvormige maan'),
+                    'de':
+                    ('Neumond',
+                     'Zunehmender Sichelmond',
+                     'Erstes Viertel',
+                     'Zunehmender Mond',
+                     'Vollmond',
+                     'Abnehmender Mond',
+                     'Letztes Viertel',
+                     'Abnehmender Sichelmond'),
+                    'fr':
+                    ('Nouvelle lune',
+                     'Premier croissant ',
+                     'Premier quartier',
+                     'Lune gibbeuse croissante',
+                     'Pleine lune',
+                     'Lune gibbeuse décroissante',
+                     'Dernier quartier',
+                     'Dernier croissant'),
                    }
 moon_phase_symbols = ('🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘')
+
+header = {'en': ('Day', 'Phase', 'Symbol', 'Name'),
+          'nl': ('Dag', 'Stand', 'Symbool', 'Naam'),
+          'de': ('Tag', 'Phase', 'Symbole', 'Name'),
+          'fr': ('Jour', 'Phase', 'Symbole', 'Nom'),
+         }
 
 
 def moon_phase_code_to_name(code, lang='en'):
@@ -69,37 +95,70 @@ def day_to_moon_phase_and_accurate_code(a, day):
     return phase_today, code_today
 
 
-tsv = open('moon-phases.tsv', 'w')
-tsv_full = open('moon-phases-full.tsv', 'w')
-tsv_all = open('moon-phases-all.tsv', 'w')
-md = open('moon-phases.md', 'w')
-md_full = open('moon-phases-full.md', 'w')
-md_all = open('moon-phases-all.md', 'w')
-tsv_header = '# Day\t# Phase\t# Symbol\t# Name\n'
-tsv.write(tsv_header)
-tsv_full.write(tsv_header)
-tsv_all.write(tsv_header)
-md_header = '''Day        | Phase  | Symbol | Name
------------|--------|--------|-----
-'''
-md.write(md_header)
-md_full.write(md_header)
-md_all.write(md_header)
-a = Astral()
-today = date.today()
-start = today - timedelta(days=31 + 1)
-end = today + timedelta(days=366 + (2 * 31))
-for i in range((end - start).days):
-    day = start + timedelta(days=i)
-    phase, code = day_to_moon_phase_and_accurate_code(a, day)
-    symbol = moon_phase_code_to_symbol(code)
-    name = moon_phase_code_to_name(code)
-    tsv_all.write('{}\t{:6.3f}\t{}\t{}\n'.format(day, phase, symbol, name))
-    md_all.write('{} | {:6.3f} | {}     | {}\n'.format(day, phase, symbol, name))
-    if code % 2 == 0:
-        tsv.write('{}\t{:6.3f}\t{}\t{}\n'.format(day, phase, symbol, name))
-        md.write('{} | {:6.3f} | {}     | {}\n'.format(day, phase, symbol, name))
-    if code == 4:
-        tsv_full.write('{}\t{:6.3f}\t{}\t{}\n'.format(day, phase, symbol, name))
-        md_full.write('{} | {:6.3f} | {}     | {}\n'.format(day, phase, symbol, name))
+def write_files(lang='en'):
+    tsv = open('moon-phases.tsv', 'w')
+    tsv_full = open('moon-phases-full.tsv', 'w')
+    tsv_all = open('moon-phases-all.tsv', 'w')
+    md = open('moon-phases.md', 'w')
+    md_full = open('moon-phases-full.md', 'w')
+    md_all = open('moon-phases-all.md', 'w')
+    tsv_header = '# {}\t# {}\t# {}\t# {}\n'.format(
+        header[lang][0].ljust(10),
+        header[lang][1],
+        header[lang][2],
+        header[lang][3])
+    tsv.write(tsv_header)
+    tsv_full.write(tsv_header)
+    tsv_all.write(tsv_header)
+    md_header = '''{} | {} | {} | {}
+-----------|-------:|---|---
+'''.format(header[lang][0].ljust(10),
+           header[lang][1].ljust(6),
+           header[lang][2],
+           header[lang][3])
+    md.write(md_header)
+    md_full.write(md_header)
+    md_all.write(md_header)
+    a = Astral()
+    today = date.today()
+    start = today - timedelta(days=31 + 1)
+    end = today + timedelta(days=366 + (2 * 31))
+    for i in range((end - start).days):
+        day = start + timedelta(days=i)
+        phase, code = day_to_moon_phase_and_accurate_code(a, day)
+        symbol = moon_phase_code_to_symbol(code)
+        name = moon_phase_code_to_name(code)
+        tsv_all.write('{}\t{:6.3f}\t{}\t{}\n'.format(day,
+                                                     phase,
+                                                     symbol,
+                                                     name))
+        md_all.write('{} | {:6.3f} | {} | {}\n'.format(day,
+                                                       phase,
+                                                       symbol,
+                                                       name))
+        if code % 2 == 0:
+            tsv.write('{}\t{:6.3f}\t{}\t{}\n'.format(day,
+                                                     phase,
+                                                     symbol,
+                                                     name))
+            md.write('{} | {:6.3f} | {} | {}\n'.format(day,
+                                                       phase,
+                                                       symbol,
+                                                       name))
+        if code == 4:
+            tsv_full.write('{}\t{:6.3f}\t{}\t{}\n'.format(day,
+                                                          phase,
+                                                          symbol,
+                                                          name))
+            md_full.write('{} | {:6.3f} | {} | {}\n'.format(day,
+                                                            phase,
+                                                            symbol,
+                                                            name))
 
+
+for lang in sorted(header.keys()):
+    if not isdir(lang):
+        makedirs(lang)
+    chdir(lang)
+    write_files(lang)
+    chdir('..')
